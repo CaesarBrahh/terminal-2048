70e853543769bfabb963268ae2f708f076c075f1
Julian Ware <julaenw@gmail.com>
2026-02-24 22:56:36 -0600

    alignment script for each arrow key

--- main.c
+++ main.c
@@ -3,6 +3,7 @@ TODO:
 - add colors to numbers
 - make zeros just an empty space
 - make values array in add_two function increase and decrease size dynamically
+- remove need to click "enter" after each move select
 */
 
 #include "stdio.h"
@@ -42,12 +43,12 @@ int main()
 		{
 			getchar();
 			getchar();
-			scanf("%i", &key);
+			key = getchar();
 		} while (key != 65 && key != 66 && key != 67 && key != 68);
 
 		// upon each click, shift and add values, restart loop
 		shift_values(key, board);
-		add_two(board);
+		//add_two(board);
 	}
 }
 
@@ -76,28 +77,80 @@ int display_board(int board[4][4])
 // ASCIIs: up -> 27, 91, 65; down -> 27, 91, 66; right -> 27, 91, 67; left -> 27, 91, 68
 int shift_values(int key, int board[4][4])
 {
-	// // stack
-	// if (key == 65) // up
-	// {
-	// 	// add all values that are equal in the column
-	// 	// stack upwards
-	// 	for (int i = 0; i < 4; i++)
-	// 	{
+	// stack
+	if (key == 65) // up
+	{
+		// align to up
+		for (int j = 0; j < 4; j++)
+		{
+			for (int i = 3; i > 0; i--)
+			{
+				if (board[i][j] != 0 && board[i-1][j] == 0)
+				{
+					board[i-1][j] = board[i][j];
+					board[i][j] = 0;
+				}
+			}
+		}	}
+	else if (key == 66) // down
+	{
+		// align to down
+		for (int j = 0; j < 4; j++)
+		{
+			for (int i = 0; i < 3; i++)
+			{
+				if (board[i][j] != 0 && board[i+1][j] == 0)
+				{
+					board[i+1][j] = board[i][j];
+					board[i][j] = 0;
+				}
+			}
+		}
+	}
+	else if (key == 67) // right
+	{
+		// add values
+		// for (int i = 0; i < 4; i++)
+		// {
+		// 	for (int j = 0; j < 3; j++)
+		// 	{
+		// 		if (board[i][j] == board[i][j+1])
+		// 		{
+		// 			board[i][j+1] = board[i][j] + board[i][j+1];
+		// 			board[i][j] = 0;
+		// 		}
+		// 		j++;
+		// 	}
+		// }
 
-	// 	}
-	// }
-	// else if (key == 66) // down
-	// {
-
-	// }
-	// else if (key == 67) // right
-	// {
-
-	// }
-	// else // left
-	// {
-
-	// }
+		// align to right
+		for (int i = 0; i < 4; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				if (board[i][j] != 0 && board[i][j+1] == 0)
+				{
+					board[i][j+1] = board[i][j];
+					board[i][j] = 0;
+				}
+			}
+		}
+	}
+	else if (key == 68) // left
+	{
+		// align to left
+		for (int i = 0; i < 4; i++)
+		{
+			for (int j = 3; j > 0; j--)
+			{
+				if (board[i][j] != 0 && board[i][j-1] == 0)
+				{
+					board[i][j-1] = board[i][j];
+					board[i][j] = 0;
+				}
+			}
+		}
+	}
 
 	return 0;
 }
