9098c61315d12847f242ee4bf77be4e3fb9f20d0
Julian Ware <julaenw@gmail.com>
2026-02-25 19:15:55 -0600

    corner anchoring

--- bot.c
+++ bot.c
@@ -6,15 +6,20 @@
 #include "bot.h"
 #include "game.h"
 
-void print_board_debug(int board[4][4])
+int MAX = 2;
+
+void find_max(int board[4][4])
 {
     for (int i = 0; i < 4; i++)
     {
         for (int j = 0; j < 4; j++)
-            printf("%4d", board[i][j]);
-        printf("\n");
+        {
+        	if (board[i][j] > MAX)
+        	{
+        		MAX = board[i][j];
+        	}
+        }
     }
-    printf("\n");
 }
 
 int best_move(int board[4][4])
@@ -47,6 +52,9 @@ int best_move(int board[4][4])
 			best_index = i;
 		}
 	}
+
+	find_max(scores[best_index].board);
+
 	printf("Up: %i, Down:%i, Right:%i, Left:%i\n", scores[0].points, scores[1].points, scores[2].points, scores[3].points);
 	usleep(200000);
 	return scores[best_index].move;
@@ -56,16 +64,23 @@ int evaluate(int board[4][4])
 {
 	int count = 0;
 
+	// check max is in bottom-right corner
+	if (board[0][0] == MAX)
+	{
+		count += 5;
+	}
+
 	// count all values in board[4][4] that are 0
 	for (int i = 0; i < 4; i++)
 	{
 		for (int j = 0; j < 4; j++)
 		{
+			// check for empty space
 			if (board[i][j] == 0)
 			{
 				count++;
 			}
-			
+
 			// compare right and up neighbor
 			if (board[i][j-1] == board[i][j])
 			{
